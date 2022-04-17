4672cee3f28146400b6110368309e613af405504
Judith Gammelli <60819967+JGammelli@users.noreply.github.com>
2022-04-17 12:08:15 +0200

    added files

---
+++ wp-content/themes/custom/template-archive.php
@@ -0,0 +1,21 @@
+<?php 
+/**
+ * Template Name: Archive
+ */
+    get_header();
+?>
+
+<main role="main" aria-label="Content">
+	<section class="pageSection">
+		<div id="post-<?php the_ID(); ?>" <?php post_class();?>>
+			<div class="pageContainer">
+				<h1><?php the_title(); ?></h1>
+				<div class="contentContainer">
+					<?php the_content(); ?>
+				</div>
+			</div>
+		</div>
+	</section>
+</main>
+
+<?php get_footer(); ?>
