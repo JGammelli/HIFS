76ae62357eb563873a96c3afd755cfb0d4e3864c
Judith Gammelli <60819967+JGammelli@users.noreply.github.com>
2022-04-18 12:56:37 +0200

    fix

--- wp-config.php
+++ wp-config.php
@@ -20,33 +20,22 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-// define( 'DB_NAME', 'hifs' );
+define( 'DB_NAME', 'hifs' );
 
-// /** Database username */
-// define( 'DB_USER', 'root' );
+/** Database username */
+define( 'DB_USER', 'root' );
 
-// /** Database password */
-// define( 'DB_PASSWORD', '' );
+/** Database password */
+define( 'DB_PASSWORD', '' );
 
-// /** Database hostname */
-// define( 'DB_HOST', 'ec2-63-35-156-160.eu-west-1.compute.amazonaws.com:5432' );
+/** Database hostname */
+define( 'DB_HOST', 'ec2-63-35-156-160.eu-west-1.compute.amazonaws.com:5432' );
 
-// /** Database charset to use in creating database tables. */
-// define( 'DB_CHARSET', 'utf8mb4' );
+/** Database charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8mb4' );
 
-// /** The database collate type. Don't change this if in doubt. */
-// define( 'DB_COLLATE', '' );
-if(isset($_ENV[`CLEARDB_DATABASE_URL`])) {
-    $db = parse_url($_ENV[`CLEARDB_DATABASE_URL`]);
-    define('DB_NAME', trim($db[`path`],`/`));
-    define('DB_USER', $db[`user`]);
-    define('DB_PASSWORD', $db[`pass`]);
-    define('DB_HOST', $db[`host`]);
-    define('DB_CHARSET', 'utf8');
-    define('DB_COLLATE', '');
-} else {
-    die('No Database credentials!');
-}
+/** The database collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
 
 /**#@+
  * Authentication unique keys and salts.
